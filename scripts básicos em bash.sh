e1f6b746a47f3f372eb34715d3844818c06db772
Neto <162625758+neptlapelota@users.noreply.github.com>
2024-08-09 11:09:40 -0300

    Add files via upload

---
+++ scripts básicos em bash.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Informação do sistema#
+
+echo sistema operacional: $(uname -a)
+
+#Qual nome da CPU?# echo cpu: $(inxi -c)
+
+#momoria total#
+
+echo memoria total: $(free -h)
+
+#espaço total do disco#
+
+echo espaço em disco total: $(df)
+
+#informações do usuario#
+
+echo Nome do Usuario: $(whoami)
+
+#nome do diretorio home#
+
+echo Diretolo Home: $(/root/home)
+
+#onde vc esta?#
+
+echo Diretorio Atual: $(pwd)
+
+#quem esta logado?#
+
+echo Usuarios Logados: $(users)
